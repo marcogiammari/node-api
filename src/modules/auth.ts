a3147ff38cec934cd61ad073fd84199390aaf650
Marco Giammari <marcogiammari@gmail.com>
2024-01-12 12:03:18 +0100

    Comparing & Hashing Passwords

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,14 @@
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export const comparePasswords = (password, hashedPassword) => {
+	return bcrypt.compare(password, hashedPassword);
+};
+
+export const hashPassword = (password) => {
+	// 5 is the salt, meaning how many times the password is hashed
+	return bcrypt.hash(password, 5);
+};
 
 export const createJWT = (user) => {
 	const token = jwt.sign(
