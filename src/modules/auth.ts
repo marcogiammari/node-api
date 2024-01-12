baaa2a79fc231012a2ea492e6da5378612a532c8
Marco Giammari <marcogiammari@gmail.com>
2024-01-12 11:29:00 +0100

    Validating a Bearer Token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -16,4 +16,24 @@ export const protect = (req, res, next) => {
 		res.json({ message: "Not authorized" });
 		return;
 	}
+
+	const [, token] = bearer.split(" "); // Bearer <token>
+
+	if (!token) {
+		res.status(401);
+		res.json({ message: "Not valid token" });
+		return;
+	}
+
+	try {
+		const user = jwt.verify(token, process.env.JWT_SECRET);
+		// attach user to request for future routes
+		req.user = user;
+		next();
+	} catch (e) {
+		console.error(e);
+		res.status(401);
+		res.json({ message: "Not valid token" });
+		return;
+	}
 };
